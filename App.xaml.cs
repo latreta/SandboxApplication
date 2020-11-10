0fd5f9e36cc3829a29e7efa0aaa188af404e82ba
latreta <yveshenr@gmail.com>
2020-11-10 03:25:14 -0300

    Starting project

---
+++ App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Unity.Windows;
+using SandboxApplication.Utils;
+using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SandboxApplication
+{
+    sealed partial class App : PrismUnityApplication
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate(PageTokens.MAIN_PAGE, null);
+            return Task.CompletedTask;
+        }
+
+        protected override UIElement CreateShell(Frame rootFrame)
+        {
+            var shell = new AppShell();
+            shell.SetFrame(rootFrame);
+            return shell;
+        }
+
+        protected override void ConfigureContainer()
+        {
+            // Configurar services e singletons da aplicação aqui
+            base.ConfigureContainer();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace SandboxApplication {
+    public sealed partial class AppShell : UserControl
+    {
+        public AppShell()
+        {
+            this.InitializeComponent();
+        }
+        public void SetFrame(Frame frame)
+        {
+            ContentFrame.Content = frame;
+        }
+    }
+}
